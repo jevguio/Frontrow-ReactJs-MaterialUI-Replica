7e7bd32e0194a3c5ae454d6fd9c14c9935e084a5
jevguio <jvguioguio@gmail.com>
2024-11-08 00:59:37 +0800

    aw

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({ 
+export default defineConfig({
+  base: 'Frontrow-ReactJs-MaterialUI-Replica',
   plugins: [react()],
   build: {
     outDir: 'build', // Ensure this matches with your deployment script expectations
